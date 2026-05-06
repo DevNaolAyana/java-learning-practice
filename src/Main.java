61010c58ca247335084322b22db05a04c767751e
DevNaolAyana <146873870+DevNaolAyana@users.noreply.github.com>
2026-05-06 20:00:55 +0300

    code for branch 02_03b

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,16 @@ public class Main {
         char studentFirstInitial = 'K';
         char studentLastInitial = 'H';
         boolean hasPerfectAttendance = true;
+        String fname = "zoraz";
+        String lname = "bapi";
+        
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
         System.out.println(studentFirstInitial);
         System.out.println(studentLastInitial);
         System.out.println(hasPerfectAttendance);
+        System.out.println(fname);
+        System.out.println(lname);
     }
 }
